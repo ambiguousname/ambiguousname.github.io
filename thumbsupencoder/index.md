378d7b98c0b5932adc0a59864da5c5ee17b3a154
Tyler K <tyly04@gmail.com>
2023-03-26 22:28:38 -0700

    Added quick encoding language

---
+++ thumbsupencoder/index.md
@@ -0,0 +1,17 @@
+---
+---
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="thumbsup.js"></script>
+        <link rel="stylesheet" href="/assets/css/stylesheet.css" />
+    </head>
+    <body>
+        <div class="content">
+            <textarea placeholder="Input" id="input"></textarea>
+            <button onclick="encode()">Encode</button>
+            <textarea placeholder="Output" id="output"></textarea>
+            <button onclick="decode()">Decode</button>
+        </div>
+    </body>
+</html>
---
+++ thumbsupencoder/thumbsup.js
@@ -0,0 +1,54 @@
+var thumbs = ['👍', '👍🏻', '👍🏼', '👍🏽', '👍🏾', '👍🏿'];
+
+function encode() {
+    var textToEncode = document.getElementById("input").value;
+    var encoder = new TextEncoder();
+    var encoded = encoder.encode(textToEncode);
+    var thumbsUpEncoded = "";
+    for (var i in encoded) {
+        var toBase6 = encoded[i].toString(6);
+        var startingZeroes = (3 - toBase6.length);
+        for (var j = 0; j < startingZeroes; j++) {
+            thumbsUpEncoded += thumbs[0];
+        } 
+        while (toBase6 !== "") {
+            var firstDigit = toBase6[0];
+            thumbsUpEncoded += thumbs[firstDigit];
+            toBase6 = toBase6.substring(1, toBase6.length);
+        }
+    }
+
+    document.getElementById("output").value = thumbsUpEncoded;
+}
+
+function decode() {
+    var textToDecode = document.getElementById("output").value;
+    var decodedTextThumbs = Array.from(textToDecode);
+    var thumbBase6 = [];
+    for (var i = 0; i < decodedTextThumbs.length; i++){
+        if (thumbs[0] == decodedTextThumbs[i]) {
+            if (i + 1 < decodedTextThumbs.length && decodedTextThumbs[i + 1] !== thumbs[0]) {
+                thumbBase6.push((decodedTextThumbs[i + 1].charCodeAt(1) - "🏻".charCodeAt(1)) + 1);
+            } else {
+                thumbBase6.push(0);
+            }
+        }
+    }
+    if (thumbBase6.length % 3 !== 0) {
+        document.getElementById("input").value = "Improperly formatted";
+        return;
+    }
+    
+    var fullLength = thumbBase6.length / 3;
+    var decoded = new Uint8Array(fullLength);
+    for (var i = 0; i < fullLength; i++) {
+        var fromBase6 = "";
+        for (var j = 0; j < 3; j++) {
+            fromBase6 += thumbBase6[i * 3 + j];
+        }
+        decoded[i] = parseInt(fromBase6, 6);
+    }
+    var decoder = new TextDecoder();
+    var decodedMessage = decoder.decode(decoded);
+    document.getElementById("input").value = decodedMessage;
+}
